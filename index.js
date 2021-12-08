61f8eafeadd30eb53ec18be72e1111886eb40981
fontilan <68748054+fontilan@users.noreply.github.com>
2021-12-08 17:59:21 +0100

    optional randomness options

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const gridGap = 20;
-const colorBackground = "#161a1d";
+const gap = 20;
+// TO RANDOMIZE THE GAP (DENSITY) A LITTLE BIT USE THE LINES BELOW:
+//
+// const gap = randomGap();
+// function randomGap() {
+//   return Math.floor(Math.random() * 10 + 15);
+// }
+//
+const lineWeight = 4;
+//
+// TO RANDOMIZE THE LINE WEIGHT A LITTLE BIT USE THE LINES BELOW:
+//
+// const lineWeight = randomWeight();
+// function randomWeight() {
+//   return Math.floor(Math.random() * 3 + 3);
+// }
+//
+const backgroundColor = "#161a1d";
 
 function randomColor() {
   return Math.floor(Math.random() * 256);
@@ -10,23 +26,23 @@ function zeroOrOne() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth * 0.999, windowHeight * 0.999);
 }
 
 function draw() {
-  background(colorBackground);
+  background(backgroundColor);
   stroke(randomColor(), randomColor(), randomColor());
-  strokeWeight(4);
-  for (y = 0; y <= windowHeight; y += gridGap) {
-    for (x = zeroOrOne() * gridGap; x <= windowWidth; x += gridGap) {
-      line(x, y, x + gridGap, y);
-      x += gridGap;
+  strokeWeight(lineWeight);
+  for (y = 0; y <= windowHeight; y += gap) {
+    for (x = zeroOrOne() * gap; x <= windowWidth; x += gap) {
+      line(x, y, x + gap, y);
+      x += gap;
     }
   }
-  for (x = 0; x <= windowWidth; x += gridGap) {
-    for (y = zeroOrOne() * gridGap; y <= windowHeight; y += gridGap) {
-      line(x, y, x, y + gridGap);
-      y += gridGap;
+  for (x = 0; x <= windowWidth; x += gap) {
+    for (y = zeroOrOne() * gap; y <= windowHeight; y += gap) {
+      line(x, y, x, y + gap);
+      y += gap;
     }
   }
   noLoop();
