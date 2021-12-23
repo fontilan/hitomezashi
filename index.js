840125e1f8b261815195ddb6939cdbfa03a2d938
fontilan <68748054+fontilan@users.noreply.github.com>
2021-12-23 20:39:19 +0100

    tidy up

--- index.js
+++ index.js
@@ -15,26 +15,23 @@ const lineWeight = 4;
 //   return Math.floor(Math.random() * 3 + 3);
 // }
 //
-const backgroundColor = "#161a1d";
-
-const zeroOrOne = [0, 1];
 
 function setup() {
   createCanvas(windowWidth * 0.999, windowHeight * 0.999);
 }
 
 function draw() {
-  background(backgroundColor);
+  background("#161a1d");
   stroke(random(50, 256), random(50, 256), random(50, 256));
   strokeWeight(lineWeight);
-  for (y = 0; y <= windowHeight; y += gap) {
-    for (x = random(zeroOrOne) * gap; x <= windowWidth; x += gap) {
+  for (y = 0; y <= height; y += gap) {
+    for (x = random([0, 1]) * gap; x <= width; x += gap) {
       line(x, y, x + gap, y);
       x += gap;
     }
   }
-  for (x = 0; x <= windowWidth; x += gap) {
-    for (y = random(zeroOrOne) * gap; y <= windowHeight; y += gap) {
+  for (x = 0; x <= width; x += gap) {
+    for (y = random([0, 1]) * gap; y <= height; y += gap) {
       line(x, y, x, y + gap);
       y += gap;
     }
